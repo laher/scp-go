bd253426940cd2a13180c5460b3897e1f55b489a
Am Laher <am@laher.net.nz>
2013-11-13 22:21:02 +1300

    recursive scp from remote

--- scp/scpfromremote.go
+++ scp/scpfromremote.go
@@ -61,124 +61,156 @@ func scpFromRemote(srcUser, srcHost, srcFile, dstFile string, options ScpOptions
 		scanner := bufio.NewScanner(r)
 		more := true
 		for more {
-			scanner.Scan()
-			err = scanner.Err()
+			cmdArr := make([]byte, 1)
+			n, err := r.Read(cmdArr)
 			if err != nil {
 				fmt.Fprintln(os.Stderr, "Error reading standard input:", err)
 				ce <- err
 				return
 			}
-			//first line
-			cmdFull := scanner.Text()
-			//first char
-			cmd := cmdFull[0]
-			//remainder, split by spaces
-			parts := strings.SplitN(cmdFull[1:], " ", 3)
-			fmt.Printf("Received command: %s (%v). Details: %v\n", string(cmd), cmd, cmdFull[1:])
+			if n < 1 {
+				fmt.Fprintln(os.Stderr, "Error reading next byte from standard input")
+				ce <- errors.New("Error reading next byte from standard input")
+				return
+			}
+			cmd := cmdArr[0]
+			fmt.Printf("Received command: %s (%v)\n", string(cmd), cmd)
 			if cmd == 0x0 {
 				//continue
-				fmt.Printf("Received OK: %s\n", cmdFull[1:])
-				err = sendByte(cw, 0)
-				if err != nil {
-					println("Write error: " + err.Error())
-					ce <- err
-					return
-				}
-			} else if cmd == 0x1 {
-				fmt.Printf("Received error: %s\n", cmdFull[2:])
-				ce <- errors.New(cmdFull[2:])
-				return
-			} else if cmd == 'D' || cmd == 'C' {
-				mode, err := strconv.ParseInt(parts[0], 8, 32)
-				if err != nil {
-					println("Format error: " + err.Error())
-					ce <- err
-					return
-				}
-				size, err := strconv.Atoi(parts[1])
-				if err != nil {
-					println("Format error: " + err.Error())
-					ce <- err
-					return
-				}
-				filename := parts[2]
-				fmt.Printf("Mode: %d, size: %d, filename: %s\n", mode, size, filename)
-				err = sendByte(cw, 0)
-				if err != nil {
-					println("Write error: " + err.Error())
-					ce <- err
-					return
-				}
-				if cmd == 'C' {
-					thisDstFile := filepath.Join(dstDir, filename)
-					println("Creating destination file: ", thisDstFile)
-					//TODO: mode here
-					fw, err := os.Create(thisDstFile)
-					if err != nil {
-						ce <- err
-						println("File creation error: " + err.Error())
-						return
-					}
-					defer fw.Close()
-
-					//todo - buffer ...
-					b := make([]byte, size)
-					_, err = r.Read(b)
-					if err != nil {
-						println("Read error: " + err.Error())
-						ce <- err
-						return
-					}
-					_, err = fw.Write(b)
-					if err != nil {
-						println("Write error: " + err.Error())
-						ce <- err
-						return
-					}
-					err = fw.Close()
-					if err != nil {
-						println(err.Error())
-						ce <- err
-						return
-					}
-					_, err = cw.Write([]byte{0})
-					if err != nil {
-						println("Write error: " + err.Error())
-						ce <- err
-						return
-					}
-					err = cw.Close()
-					if err != nil {
-						println(err.Error())
-						ce <- err
-						return
-					}
-				} else { //D command (directory)
-					thisDstFile := filepath.Join(dstDir, filename)
-					fileMode := os.FileMode(uint32(mode))
-					err = os.Mkdir(thisDstFile, fileMode)
-					if err != nil {
-						println("Mkdir error: " + err.Error())
-						os.Exit(1)
-						ce <- err
-						return
-					}
-					dstDir = thisDstFile
-				}
-			} else if cmd == 'E' { //E command: go back out of dir
-				dstDir = filepath.Dir(dstDir)
-/*
-				err = sendByte(cw, 0)
+				fmt.Printf("Received OK \n")
+		/*		err = sendByte(cw, 0)
 				if err != nil {
 					println("Write error: " + err.Error())
 					ce <- err
 					return
 				}
 */
-			} else {
-				fmt.Printf("Command '%v' NOT implemented\n", cmd)
+			} else if cmd == 'E' { //E command: go back out of dir
+				dstDir = filepath.Dir(dstDir)
+				fmt.Printf("Received End-Dir\n")
+				err = sendByte(cw, 0)
+				if err != nil {
+					println("Write error: " + err.Error())
+					ce <- err
+					return
+				}
+
+			} else if cmd == 0xA { //0xA command: end?
+				fmt.Printf("Received All-done\n")
+				err = sendByte(cw, 0)
+				if err != nil {
+					println("Write error: " + err.Error())
+					ce <- err
+					return
+				}
 				return
+			} else {
+				scanner.Scan()
+				err = scanner.Err()
+				if err != nil {
+					fmt.Fprintln(os.Stderr, "Error reading standard input:", err)
+					ce <- err
+					return
+				}
+				//first line
+				cmdFull := scanner.Text()
+				fmt.Printf("Details: %v\n", cmdFull)
+				//remainder, split by spaces
+				parts := strings.SplitN(cmdFull, " ", 3)
+
+				if cmd == 0x1 {
+					fmt.Printf("Received error: %s\n", cmdFull[1:])
+					ce <- errors.New(cmdFull[1:])
+					return
+				} else if cmd == 'D' || cmd == 'C' {
+					mode, err := strconv.ParseInt(parts[0], 8, 32)
+					if err != nil {
+						println("Format error: " + err.Error())
+						ce <- err
+						return
+					}
+					size, err := strconv.Atoi(parts[1])
+					if err != nil {
+						println("Format error: " + err.Error())
+						ce <- err
+						return
+					}
+					filename := parts[2]
+					fmt.Printf("Mode: %d, size: %d, filename: %s\n", mode, size, filename)
+					err = sendByte(cw, 0)
+					if err != nil {
+						println("Send error: " + err.Error())
+						ce <- err
+						return
+					}
+					if cmd == 'C' {
+						thisDstFile := filepath.Join(dstDir, filename)
+						println("Creating destination file: ", thisDstFile)
+						//TODO: mode here
+						fw, err := os.Create(thisDstFile)
+						if err != nil {
+							ce <- err
+							println("File creation error: " + err.Error())
+							return
+						}
+						defer fw.Close()
+
+						//todo - buffer ...
+						b := make([]byte, size)
+						_, err = r.Read(b)
+						if err != nil {
+							println("Read error: " + err.Error())
+							ce <- err
+							return
+						}
+						_, err = fw.Write(b)
+						if err != nil {
+							println("Write error: " + err.Error())
+							ce <- err
+							return
+						}
+						err = fw.Close()
+						if err != nil {
+							println(err.Error())
+							ce <- err
+							return
+						}
+						nb := make([]byte, 1)
+						_, err = r.Read(nb)
+						if err != nil {
+							println(err.Error())
+							ce <- err
+							return
+						}
+						_, err = cw.Write([]byte{0})
+						if err != nil {
+							println("Send null-byte error: " + err.Error())
+							ce <- err
+							return
+						}
+					} else { //D command (directory)
+						thisDstFile := filepath.Join(dstDir, filename)
+						fileMode := os.FileMode(uint32(mode))
+						err = os.MkdirAll(thisDstFile, fileMode)
+						if err != nil {
+							println("Mkdir error: " + err.Error())
+							os.Exit(1)
+							ce <- err
+							return
+						}
+						dstDir = thisDstFile
+					}
+				} else {
+					fmt.Printf("Command '%v' NOT implemented\n", cmd)
+					return
+				}
 			}
+		}
+		err = cw.Close()
+		if err != nil {
+			println("error closing process writer: ", err.Error())
+			ce <- err
+			return
 		}
 	}()
 	remoteOpts := "-qf";
